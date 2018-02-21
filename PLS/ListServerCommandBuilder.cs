012345cca05af04a3f82e3a13d75ccf36ec591a9
Loup PELUSO <loup.peluso@alcuin.com>
2018-02-21 14:07:33 +0100

    WIP

---
+++ PLS/ListServerCommandBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace PLS
+{
+    public class ListServerCommandBuilder : ICommandBuilder
+    {
+        private readonly PlsDbContext _db;
+
+        public ListServerCommandBuilder(PlsDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Apply(CommandLineApplication target)
+        {
+            target.Command("list", command =>
+            {
+                command.AddHelp();
+
+                command.OnExecute(() =>
+                {
+                    foreach (var id in _db.Servers.Select(_ => _.Id).ToArray())
+                    {
+                        Console.WriteLine(id);
+                    }
+                    return 0;
+                });
+            });
+        }
+    }
+}
