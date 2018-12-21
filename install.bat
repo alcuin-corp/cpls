bdaad1a11d1f19e4d105125d4b41ee596004267e
Loup Peluso <loup.peluso@gmail.com>
2018-12-21 19:57:37 +0100

    Fixes install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
-dotnet pack --output ./
+dotnet pack -c Release --output ./
 dotnet tool uninstall -g Alcuin.PLS
-dotnet tool install -g Alcuin.PLS --add-source ./
+dotnet tool install -g Alcuin.PLS --version 1.0.0 --add-source ./
